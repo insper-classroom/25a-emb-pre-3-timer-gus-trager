765801691ab0cbbdb6e5be7fbefe8543a8b88bc4
Your Name <you@example.com>
2025-03-10 01:34:03 -0300

    Ex2

--- exe2/main.c
+++ exe2/main.c
@@ -11,6 +11,9 @@ const int LED_PIN_G = 6;
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
+volatile int g_timer_r = 0;
+volatile int g_timer_g = 0;
+
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {
         if (gpio == BTN_PIN_R)
@@ -18,6 +21,16 @@ void btn_callback(uint gpio, uint32_t events) {
         else if (gpio == BTN_PIN_G)
             flag_g = 1;
     }
+}
+
+bool timer_r_callback(repeating_timer_t *rt) {
+    g_timer_r = 1;
+    return true; // keep repeating
+}
+
+bool timer_g_callback(repeating_timer_t *rt) {
+    g_timer_g = 1;
+    return true; // keep repeating
 }
 
 int main() {
@@ -39,14 +52,57 @@ int main() {
                                        &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
+    repeating_timer_t timer_r;
+    repeating_timer_t timer_g;
+
+    bool timer_r_ativo = false;
+    bool timer_g_ativo = false;
+
     while (true) {
 
         if (flag_r) {
             flag_r = 0;
+            if (!timer_r_ativo) {
+                if (!add_repeating_timer_ms(500, timer_r_callback, NULL, &timer_r)) {
+                    printf("Failed to add timer\n");
+                }
+                else{
+                    timer_r_ativo = true;
+                }
+            }
+            else {
+                cancel_repeating_timer(&timer_r);
+                timer_r_ativo = false;
+                gpio_put(LED_PIN_R, 0);
+            }
         }
 
         if (flag_g) {
             flag_g = 0;
+            if (!timer_g_ativo) {
+                if (!add_repeating_timer_ms(250, timer_g_callback, NULL, &timer_g)) {
+                    printf("Failed to add timer\n");
+                }
+                else{
+                    timer_g_ativo = true;
+                }
+            }
+            else {
+                cancel_repeating_timer(&timer_g);
+                timer_g_ativo = false;
+                gpio_put(LED_PIN_G, 0);
+            }
+        }
+
+        if (g_timer_r) {
+            g_timer_r = 0;
+            gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+        }
+
+        if (g_timer_g) {
+            g_timer_g = 0;
+            gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G));
         }
     }
 }
+
